7f65fa79ca9270e2beae2cb18bc068a13c4c8535
Jeremiah <sentomerojeremy@gmail.com>
2025-06-16 15:23:56 +0300

    new structure and connection

---
+++ backend/database/connect.php
@@ -0,0 +1,19 @@
+<?php
+require_once __DIR__ . '/vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+$host = getenv('DB_HOST');
+$user = getenv('DB_USER');
+$password = getenv('DB_PASSWORD');
+$database = getenv('DB_NAME');
+
+$conn = new mysqli($host, $user, $password, $database);
+if ($conn->connect_error) {
+    die("Connection failed: " . $connection->connect_error);
+}else{
+    echo "Connected successfully to the database.";
+}
+
+?>
